0a59db800c13c95a9af2f3d49faaaa3452b8e1d9
julianoLenovo <juliano@localhost.localdomain>
2016-08-12 10:35:44 -0300

    Change header and footer image. Adds fixed width for button in ram.php file

--- ram.php
+++ ram.php
@@ -12,34 +12,51 @@
 	    <div class="container">
 			<div class="row">
 				<div class="col-lg-8 col-lg-offset-2">							
-					<h4>Programming and Linux </h4>					
+					<h4>Programming and Linux </h4>	
+					<div class="smpFix">
 					<p>
-					<p>
-					<a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+					<a class="btn btn-primary btn-block" data-toggle="collapse" href="#cllps1" aria-expanded="false" aria-controls="cllps1">
 					Linux / Debian
 					</a>					
 					</p>
-					<div class="collapse" id="collapseExample">
-					<div class="card card-block">
-					<ul class="list-group">
-					  
-					  <li class="list-group-item">&#8594;<a href="http://sft.if.usp.br/debian-cd/">Debian</a> - Linux OS I am currently using. I use the GNOME desktop environment.</li>
-					  <li class="list-group-item">&#8594;<a href="https://wiki.debian.org/PepperFlashPlayer/Installing">PepperFlashPlayer</a> - A different version of the Adobe Flash Player that works on Chromium and IceWeasel. It has improved security standarts than the traditional Flash player.</li>	
-					  <li class="list-group-item">&#8594;<a href="https://github.com/robbyrussell/oh-my-zsh">Oh-my-zsh</a> - Oh my Zsh is a framework to use with the Zsh shell (which is a much better shell than Bash). Oh-my-zsh adds support to plugins, easy configurable themes and much more.</li>					  
-					  <li class="list-group-item">&#8594;<a href="https://github.com/bhilburn/powerlevel9k">PowerLevel9K</a> - A theme / plugin for the ZSH shell. Works perfectly with Oh-my-zsh, fully configurable. </li>
-					  <li class="list-group-item">&#8594;<a href="https://github.com/unixorn/awesome-zsh-plugins">awesome-zsh-plugins</a> - List of great plugins for all kinds of needs. Install it with oh-my-zsh.	 </li>
-					  <li class="list-group-item">&#8594;<a href=""></a> - </li>
-					  <li class="list-group-item">&#8594;<a href=""></a> - </li>
-					</ul>
-					</div>
 					</div>
 
+					<div class="collapse" id="cllps1">
+						<div class="card card-block">
+							<ul class="list-group">
+							  
+							  <li class="list-group-item">&#8594;<a href="http://sft.if.usp.br/debian-cd/">Debian</a> - Linux OS I am currently using. I use the GNOME desktop environment.</li>
+							  <li class="list-group-item">&#8594;<a href="https://wiki.debian.org/PepperFlashPlayer/Installing">PepperFlashPlayer</a> - A different version of the Adobe Flash Player that works on Chromium and IceWeasel. It has improved security standarts than the traditional Flash player.</li>	
+							  <li class="list-group-item">&#8594;<a href="https://github.com/robbyrussell/oh-my-zsh">Oh-my-zsh</a> - Oh my Zsh is a framework to use with the Zsh shell (which is a much better shell than Bash). Oh-my-zsh adds support to plugins, easy configurable themes and much more.</li>					  
+							  <li class="list-group-item">&#8594;<a href="https://github.com/bhilburn/powerlevel9k">PowerLevel9K</a> - A theme / plugin for the ZSH shell. Works perfectly with Oh-my-zsh, fully configurable. </li>
+							  <li class="list-group-item">&#8594;<a href="https://github.com/unixorn/awesome-zsh-plugins">awesome-zsh-plugins</a> - List of great plugins for all kinds of needs. Install it with oh-my-zsh.	 </li>
+							  <li class="list-group-item">&#8594;<a href="http://askubuntu.com/questions/226061/how-to-install-the-boot-repair-tool-in-an-ubuntu-live-disc">Boot Repair</a> - An amazing utility to repair GRUB2 boot problems, very useful for dual boot users. </li>						  
+							</ul>
+						</div>
+					</div>
 
+					<div class="smpFix">
+					<p>
+					<a class="btn btn-primary btn-block" data-toggle="collapse" href="#cllps2" aria-expanded="false" aria-controls="cllps2">
+					Python
+					</a>					
+					</p>
+					</div>	
 
-					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-					<p><a href="blog01.html">Continue Reading...</a></p>
+					<div class="collapse" id="cllps2">
+						<div class="card card-block">
+							<ul class="list-group">
+								<li class="list-group-item">&#8594;<a href=""></a> - </li>
+								<li class="list-group-item">&#8594;<a href=""></a> - </li>
+								<li class="list-group-item">&#8594;<a href=""></a> - </li>
+
+							</ul>
+						</div>
+					</div>
+
+					
+
 				</div>
-<li class="list-group-item"><a href=""></a> - </li>
 			</div><!-- /row -->
 	    </div> <!-- /container -->
 	</div><!-- /white -->
