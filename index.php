015121446d3deb085715c432107b4d5b1654f0be
Juliano Garcia <julianogarcia_1997@hotmail.com>
2016-10-08 14:38:51 -0300

    Add gitignore

--- index.php
+++ index.php
@@ -1,28 +1,29 @@
 <?php include 'header.php' ?>
 
+<h1 class="centered">Ｒｏｂｏｔｅｎｉｑｕｅ's  	&nbsp; 	&nbsp;Ｈｏｍｅ~</h1>
 
 	<!-- +++++ Welcome Section +++++ -->
 	<div id="ww">
+
 	    <div class="container">
 			<div class="row">
 				<div class="col-lg-8 col-lg-offset-2 centered">
-					<img src="assets/img/user.png" alt="Robotenique" width="auto" height="auto">
-					<h1>Welcome to the void~</h1>
+                    <img border="0" class="img-responsive" alt="myHome" src="assets/img/blog01.jpg">
 					<p>Hello everybody, this is my personal page. Here I write some interesting things and sometimes share my programming exercises and other programming I do sometimes :)    </p>
 					<h4>Feel free to check my GitHub ➙ <a href="https://github.com/robotenique">
-<img border="0" alt="myGit" src="assets/img/GitHub-Mark-120px-plus.png" width="100" height="100">
+                    <img border="0" alt="myGit" src="assets/img/GitHub-Mark-120px-plus.png" width="100" height="100">
 </a></h4>
-				
+
 				</div><!-- /col-lg-8 -->
 			</div><!-- /row -->
 	    </div> <!-- /container -->
 	</div><!-- /ww -->
-	
-	
+
+
 	<!-- +++++ Projects Section +++++ -->
-	
+
 	<div class="container pt">
-		<div class="row mt centered">	
+		<div class="row mt centered">
 			<div class="col-lg-4">
 				<a class="zoom green" href="javascript:void()"><img class="img-responsive" src="assets/img/matrix.gif" alt="" /></a>
 				<p>† عميق MOLLIWATER † </p>
@@ -36,7 +37,7 @@
 				<p>± YUNG SL1ME ± </p>
 			</div>
 		</div><!-- /row -->
-		<div class="row mt centered">	
+		<div class="row mt centered">
 			<div class="col-lg-4">
 				<a class="zoom green" href="javascript:void()"><img class="img-responsive" src="assets/img/matrix.gif" alt="" /></a>
 				<p>//◭// の男 S§S //◭// </p>
@@ -51,11 +52,11 @@
 			</div>
 		</div><!-- /row -->
 	</div><!-- /container -->
-	
-	
+
+
 	<?php include 'footer.php'; ?>
 
-	
+
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
