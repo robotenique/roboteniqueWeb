cabb2f43c7594ff4f542236185799b7a763eff90
Juliano Garcia <julianogarcia_1997@hotmail.com>
2016-10-08 14:39:27 -0300

    Add new home image and reduce homepage height

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
 	    <div class="container">
 			<div class="row">
 				<div class="col-lg-8 col-lg-offset-2 centered">
-                    <img border="0" class="img-responsive" alt="myHome" src="assets/img/blog01.jpg">
+                    <img border="0" class="img-responsive" alt="myHome" src="assets/img/robovoid.jpg">
 					<p>Hello everybody, this is my personal page. Here I write some interesting things and sometimes share my programming exercises and other programming I do sometimes :)    </p>
 					<h4>Feel free to check my GitHub ➙ <a href="https://github.com/robotenique">
-                    <img border="0" alt="myGit" src="assets/img/GitHub-Mark-120px-plus.png" width="100" height="100">
+                    <img border="0" alt="myGit" src="assets/img/robt.png" width="100" height="100">
 </a></h4>
 
 				</div><!-- /col-lg-8 -->
@@ -22,21 +22,7 @@
 
 	<!-- +++++ Projects Section +++++ -->
 
-	<div class="container pt">
-		<div class="row mt centered">
-			<div class="col-lg-4">
-				<a class="zoom green" href="javascript:void()"><img class="img-responsive" src="assets/img/matrix.gif" alt="" /></a>
-				<p>† عميق MOLLIWATER † </p>
-			</div>
-			<div class="col-lg-4">
-				<a class="zoom green" href="javascript:void()"><img class="img-responsive" src="assets/img/matrix.gif" alt="" /></a>
-				<p>☥ シ EYEZ ☥ </p>
-			</div>
-			<div class="col-lg-4">
-				<a class="zoom green" href="javascript:void()"><img class="img-responsive" src="assets/img/matrix.gif" alt="" /></a>
-				<p>± YUNG SL1ME ± </p>
-			</div>
-		</div><!-- /row -->
+	<div class="container pt">		
 		<div class="row mt centered">
 			<div class="col-lg-4">
 				<a class="zoom green" href="javascript:void()"><img class="img-responsive" src="assets/img/matrix.gif" alt="" /></a>
