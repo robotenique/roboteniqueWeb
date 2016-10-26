cc3d161d3a5fcec6eee5813b09637d2fa6462838
Juliano - Robotenique <julianogarcia_1997@hotmail.com>
2016-10-26 11:50:51 -0200

    Add Bernoulli Theorem website to linux server

--- prog/wklaw.php
+++ prog/wklaw.php
@@ -6,7 +6,7 @@
 	    <div class="container">
 			<div class="row">
 				<div class="col-lg-8 col-lg-offset-2 centered">
-					<h1>Bernoulli's theorem Simulator</h1>
+					<a href="wk/"><h1>Bernoulli's theorem Simulator</h1></a>
 					<h4>Dice graph</h4>
 					<img class="img-responsive" src="../assets/img/portfolio/wklaw2.png" alt="" />
 					<h4>Coin graph</h4>
@@ -25,6 +25,7 @@
 				<h4>Bernoulli's theorem Simulator (Weak Law of Large Numbers)</h4>
 				<p align="justify">This is a project to demonstrate in graph plotting and real data,  <a href="http://link.springer.com/referenceworkentry/10.1007%2F978-0-387-32833-1_28">Weak Law of Large numbers</a>, which is a very useful Theorem used in statistics. As our website says: " In practice, it (the Weak Law of Large Numbers) dictates that, if repeated enough times, the accumulate results of the same experiment will tend to its real mathematical probabilities."</p>
 				<p align="justify">The website plots in real time the graph of throwing a dice or a coin the number of times the user specifies, so it's completely interactive, and the user have fully control over the start number, the amount of throws to perform, and even the speed of the plot. Also, the library that we use (<i>Canvas JS</i>) allow the user to see in real time the convergence of the probability.</p>
+				<p align="justify"> <a href="wk/">Click here</a> to access the website!</p>
 				<p align="justify">It was developed in partnership with <a href="https://linux.ime.usp.br/~razgrizone/">Pedro Pereira</a> in 2016. <a href="https://github.com/GreaterNumbersLaw/website"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
 				<img border="0" alt="myGit" src="../assets/img/GitHub-Mark-120px-plus.png" width="50" height="50"></a></p>
 			</div><!-- /colg-lg-6 -->
